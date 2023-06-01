562ac00545ccd2defd7ef2ddc600b80b165f0d4b
VieiraGui147 <77821542+Vieirinhagui@users.noreply.github.com>
2023-06-01 14:28:39 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,34 @@
+$(".menu-hamburguer").click(function () {
+  toggleMenu(true);
+});
 
-document.getElementById("menu-hamburguer").onclick = function() {
-    document.getElementById("background").style.display = "flex";
+$("#icon-fechar, .click").click(function () {
+  toggleMenu(false);
+});
+
+function toggleMenu(open) {
+  if (open) {
+    $(".menu-mobile").removeClass("right");
+    $("#icon-fechar").removeClass("right");
+    $("#background").css("display", "flex");
+  } else {
+    $(".menu-mobile").addClass("right");
+    $("#icon-fechar").addClass("right");
+    setTimeout(function () {
+      $("#background").css("display", "none");
+    }, 800);
+  }
 }
-document.getElementById("icon-fechar").onclick = function() {
-    document.getElementById("background").style.display = "none";
-}
+const swiper1 = new Swiper(".swiper", {
+  slidesPerView: 2,
+  loop: true,
+  speed: 5000,
+  disableOnInteraction: false,
+  autoplay: {
+    delay: 3000,
+  },
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+});
